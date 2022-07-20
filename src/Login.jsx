249ccb4b06fe3112da217138957fc9439d384b30
Muhammad Rahim <l181184@lhr.nu.edu.pk>
2022-07-20 18:18:41 +0500

    Login Feature Implemented

---
+++ src/Login.jsx
@@ -0,0 +1,35 @@
+import React, { useState,useEffect } from "react";
+import { useSelector,useDispatch } from "react-redux/es/exports";
+
+import { loginUser } from "./registrationSlice";
+const Login=()=>{
+    const loginListener=useSelector((state)=>state.registration.current_user);
+    const dispatch=useDispatch();
+    const [email,setEmail]=useState("");
+    const [password,setPassword]=useState("");
+    useEffect(() => {
+        console.log("here");
+        if(loginListener !== ""){
+            alert("Login Successful")
+        }
+    }, [loginListener])
+    
+    return(
+        <div style={{textAlign:"center", backgroundColor:"purple"}}>
+            <h1>User Login</h1>
+            <input type="text" value={email} placeholder="Email" onChange={(e)=> setEmail(e.target.value)} /><br/>
+            <input type="password" value={password} placeholder="Password" onChange={(e)=> setPassword(e.target.value)} /><br/>
+            <button onClick={()=>{
+                if(email !== "" && password!==""){
+                    dispatch(loginUser({email:email,password:password}))
+                    setEmail("")
+                    setPassword("")
+
+                }else{
+                    alert("Please fill all fields")
+                }
+            }}>Login</button>
+        </div>
+    )
+}
+export default Login;
---
+++ src/registrationSlice.js
@@ -0,0 +1,71 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState={
+    current_user: "",
+    Users:[
+        {
+            UserId:0,
+            Name:"Rahim",
+            Email:"rahim123@gmail.com",
+            Password:"123456"
+        },
+        {
+            UserId:1,
+            Name:"Ahmed",
+            Email: "ahmed@gmail.com",
+            Password:"123456"
+        }
+    ]
+}
+let ID=2;
+const registrationSlice=createSlice(
+    {
+        name:"registration",
+        initialState,
+        reducers:{
+            addUser(state,action){
+                let flag=false;
+                for(let i=0;i<state.Users.length();i++){
+                    if(state.Users[i].Email===action.payload.email){
+                        flag=true;
+                        break;
+                    }
+                }
+                if(!flag){
+                    state.Users.push({
+                        UserId:ID++,
+                        Name:action.payload.name,
+                        Email:action.payload.email,
+                        Password:action.payload.password
+                    })
+                    alert("User registered")
+                }else{
+                    alert("User already exist")
+                }
+            },
+            loginUser(state,action){
+                let flag=false;
+                for(let i=0;i<state.Users.length;i++){
+                    if(state.Users[i].Email===action.payload.email && state.Users[i].Password===action.payload.password){
+                        flag=true;
+                        break;
+                    }
+                }
+                console.log(flag);
+                if(flag){
+                    state.current_user=action.payload.email
+                }else{
+                    alert("Email or password is not correct")
+                }
+
+
+            }
+        }
+
+    }
+)
+export const {
+    addUser,
+    loginUser
+  } = registrationSlice.actions;
+  export default registrationSlice.reducer;
